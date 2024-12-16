dfb9477b1bd9c7fb6c1d7d448cd33061d2f7bc1b
22015564 <74170921+XiaoA-qwq@users.noreply.github.com>
2024-12-16 19:07:10 +0800

    首次fork修改

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 version=$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}' | sed 's/ (.*//')
 
 if [ "$isAlpha" = true ]; then
-    filename="Surfing_${version}_alpha.zip"
+    filename="Clash_${version}_alpha.zip"
 else
-    filename="Surfing_${version}_release.zip"
+    filename="Clash_${version}_release.zip"
 fi
 
 zip -r -o -X -ll "$filename" ./ -x '.git/*' -x '.github/*' -x 'folder/*' -x 'build.sh' -x 'Surfing.json' -x 'Vers.md'

bfa51efe5c95be0f8246b0349256a5b138964931
22015564 <74170921+XiaoA-qwq@users.noreply.github.com>
2024-12-20 21:43:46 +0800

    v1.0.1

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ version=$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}' | sed 's/ 
 if [ "$isAlpha" = true ]; then
     filename="Clash_${version}_alpha.zip"
 else
-    filename="Clahs_${version}_release.zip"
+    filename="Clash_${version}_release.zip"
 fi
 
 zip -r -o -X -ll "$filename" ./ -x '.git/*' -x '.github/*' -x 'folder/*' -x 'build.sh' -x 'Surfing.json' -x 'Vers.md'
--- changelog.md
+++ changelog.md
@@ -1,2 +1,3 @@
-## v6.8.16
+## v1.0.1
 - 同步 MoGuangYu/Surfing v6.8.16 版本
+- 修改config.yaml,删除多余定向规则，添加GFW
